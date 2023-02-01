184b21ba2ab78c27c179393f479bdc2aafd276c2
Karan <>
2023-01-31 21:35:40 -0500

    test4

--- main.tf
+++ main.tf
@@ -17,4 +17,23 @@ resource "aws_s3_bucket_website_configuration" "website-config" {
   }
 }
 
+resource "aws_s3_bucket_policy" "public-access" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.access-policy.json
+}
+
+data "aws_iam_policy_document" "access-policy" {
+  statement {
+     actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*",
+    ]
+  }
+}
+
 
