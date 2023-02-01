6a2af9653879915960c97ecaff944b5fb8c4a74f
Karan <>
2023-01-31 22:08:01 -0500

    test8

--- main.tf
+++ main.tf
@@ -36,3 +36,22 @@ resource "aws_s3_bucket_policy" "public_read_access" {
 }
 EOF
 }
+
+
+resource "aws_s3_object" "object" {
+  bucket = "test21121007"
+  key    = "buddha.jpg"
+  source = "./buddha.jpg"
+  etag = filemd5("./buddha.jpg")
+
+  depends_on = [aws_s3_bucket.bucket]
+}
+
+resource "aws_s3_object" "index" {
+  bucket = "test21121007"
+  key    = "index.html"
+  source = "./index.html"
+  etag = filemd5("./buddha.jpg")
+
+  depends_on = [aws_s3_bucket.bucket]
+}
