33d83606d53ffa3a3c80c8dd4184e736d68291b1
Karan <>
2023-01-31 23:23:26 -0500

    final

--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
+##############################################################################################
+#Create S3 bucket, upload objects
 resource "aws_s3_bucket" "bucket" {
    bucket = "test21121007"
    tags = {
@@ -5,6 +7,52 @@ resource "aws_s3_bucket" "bucket" {
    }
 }
 
+resource "aws_s3_bucket_website_configuration" "website-config" {
+  bucket = aws_s3_bucket.bucket.bucket
+  index_document {
+       suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "public_read_access" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+	   "Principal": "*",
+      "Action": [ "s3:*" ],
+      "Resource": [
+        "arn:aws:s3:::test21121007/*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_s3_object" "object" {
+  bucket = "test21121007"
+  key    = "buddha.jpg"
+  source = "./buddha.jpg"
+  content_type = "image/jpeg"
+  etag = filemd5("./buddha.jpg")
+
+  depends_on = [aws_s3_bucket.bucket]
+}
+
+resource "aws_s3_object" "index" {
+  bucket = "test21121007"
+  key    = "index.html"
+  source = "./index.html"
+  content_type = "text/html"
+  etag = filemd5("./index.html")
+
+  depends_on = [aws_s3_bucket.bucket]
+}
 output static-website {
    value = "http://${aws_s3_bucket.bucket.id}.s3-website-${var.region}.amazonaws.com"
 }
