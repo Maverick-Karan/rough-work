930abd919273158e1632dc83dfed3044b0320a54
Karan <>
2023-01-31 21:27:57 -0500

    test3

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_s3_bucket_website_configuration" "website-config" {
-  bucket = data.aws_s3_bucket.bucket.bucket
+  bucket = aws_s3_bucket.bucket.bucket
   index_document {
        suffix = "index.html"
   }
